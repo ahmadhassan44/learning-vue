c8dddf6ddfeb12ae32afce298ab4e2d45196e544
Ahmad Hassan <146438862+ahmadhassan44@users.noreply.github.com>
2025-05-02 22:23:42 +0500

    feat/learn composition api, directives, events, methods and reacrivity

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,37 @@
 <template>
   <h1>
-    Vue Jobs
+    {{ name }}
   </h1>
+  <p v-if="status === 'true'">{{ name }} is active</p>
+  <p v-else-if="status === 'false'">{{ name }} is inactive</p>
+  <p v-else>{{ name }}'s status is unknown</p>
+  <li v-for="task in tasks" :key="task">
+    {{ task }}
+  </li>
+  <a :href="link" target="_blank">Google</a>
+  <button @click="toggleActivity" style="display: block">
+    Toggle Activity
+  </button>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "John Doe",
+      status: "true",
+      tasks: ["Task 1", "Task 2", "Task 3", "Task 4"],
+      link: "https://google.com",
+    };
+  },
+  methods: {
+    toggleActivity() {
+      if (this.status === "true") {
+        this.status = "false";
+      } else {
+        this.status = "true";
+      }
+    },
+  },
+};
+</script>
