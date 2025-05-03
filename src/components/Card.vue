b11039f391d2f8221ad5f8a554255fecdde4622c
Ahmad Hassan <146438862+ahmadhassan44@users.noreply.github.com>
2025-05-03 22:29:54 +0500

    feat/add router links

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,8 +2,8 @@
   <div :class="[cardData.cardColor, 'p-6 rounded-lg shadow-md']">
     <h2 class="text-2xl font-bold">{{ cardData.title }}</h2>
     <p class="mt-2 mb-4">{{ cardData.subtitle }}</p>
-    <a
-      href="jobs.html"
+    <RouterLink
+      :to="cardData.to"
       :class="[
         cardData.buttonColor,
         `hover:${cardData.buttonHoverColor}`,
@@ -11,11 +11,11 @@
       ]"
     >
       {{ cardData.buttonText }}
-    </a>
+    </RouterLink>
   </div>
 </template>
 <script setup lang="ts">
-import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
 const props = defineProps({
   cardType: {
     type: String,
@@ -31,5 +31,6 @@ const cardData = {
   cardColor: props.cardType === "dev" ? "bg-gray-100" : "bg-green-100 ",
   buttonColor: props.cardType === "dev" ? "bg-black" : "bg-green-500",
   buttonHoverColor: props.cardType === "dev" ? "bg-gray-700" : "bg-green-600",
+  to: props.cardType === "dev" ? "jobs" : "jobs/add",
 };
 </script>
