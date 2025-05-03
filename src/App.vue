2991e18adab3cd8198377ca3c399013bdd9a0810
Ahmad Hassan <146438862+ahmadhassan44@users.noreply.github.com>
2025-05-03 14:29:13 +0500

    feat/add job listing component with proper props

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <Navbar />
   <Hero title="Are you a vue dev?" subtitle="We have got you covered!" />
   <HomeCard />
+  <JobListings />
 </template>
 
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
 import HomeCard from "./components/HomeCard.vue";
+import JobListings from "./components/JobListings.vue";
 </script>
